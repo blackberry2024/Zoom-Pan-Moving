c7210ea311ea794cfc16c2d6c542b4e34d0dbaeb
blackberry2024 <berathalit870@gmail.com>
2024-05-17 06:27:16 -0400

    Zoom Pan Moving with JavaScript
    Complete

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const container = document.getElementById('overlay1');
+const image = document.getElementById('zoomable-image1');
+
+console.log(container, image);
+
+addZoomPan({ container, image });
